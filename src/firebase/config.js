c9b55460a5bdff717b0f647b42a28aa9c974da11
Daniel <ruizg92@gmail.com>
2021-04-12 14:27:38 -0500

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAwxfIel3lI12EhEIYHN__u31P_JbPJ3TQ",
+    authDomain: "restaurant-fcd05.firebaseapp.com",
+    databaseURL: "https://restaurant-fcd05-default-rtdb.firebaseio.com",
+    projectId: "restaurant-fcd05",
+    storageBucket: "restaurant-fcd05.appspot.com",
+    messagingSenderId: "300066698885",
+    appId: "1:300066698885:web:79521f8088607904b69bec",
+    measurementId: "G-23RGV0QY6M"
+  };
+
+  export default firebaseConfig;
